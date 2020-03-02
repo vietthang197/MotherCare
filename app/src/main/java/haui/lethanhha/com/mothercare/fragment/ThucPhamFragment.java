b115e72e0e38b89dc41e5bd04d7c04f127e72910
Lê Việt Thắng <levietthang1997@gmail.com>
2020-03-02 21:22:38 +0700

    update code

---
+++ app/src/main/java/haui/lethanhha/com/mothercare/fragment/ThucPhamFragment.java
@@ -0,0 +1,21 @@
+package haui.lethanhha.com.mothercare.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import haui.lethanhha.com.mothercare.R;
+
+public class ThucPhamFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_an_uong, container, false);
+    }
+}
