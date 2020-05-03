d259b08bd983b8d9b8f8948c147e172f8f9cb38e
Lê Việt Thắng <levietthang1997@gmail.com>
2020-05-03 19:02:14 +0700

    update

--- app/src/main/java/haui/lethanhha/com/mothercare/activity/AnUongActivity.java
+++ app/src/main/java/haui/lethanhha/com/mothercare/activity/AnUongActivity.java
@@ -3,15 +3,20 @@ package haui.lethanhha.com.mothercare.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import haui.lethanhha.com.mothercare.R;
 import haui.lethanhha.com.mothercare.adapter.TabAdapter;
 import haui.lethanhha.com.mothercare.fragment.ThucPhamFragment;
+import haui.lethanhha.com.mothercare.model.AnUong;
 
 public class AnUongActivity extends AppCompatActivity {
 
@@ -32,9 +37,16 @@ public class AnUongActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.viewPagerAnUong);
         tabLayout =  findViewById(R.id.tabLayoutAnUong);
         adapter = new TabAdapter(getSupportFragmentManager(), this);
-        adapter.addFragment(new ThucPhamFragment(), "Thực phẩm", R.drawable.mushroom);
-        adapter.addFragment(new ThucPhamFragment(), "Món ăn", R.drawable.muffin);
-        adapter.addFragment(new ThucPhamFragment(), "Nhóm chất", R.drawable.chart);
+
+        List<AnUong> anUongList = new ArrayList<>();
+        anUongList.add(new AnUong(R.drawable.atiso, R.drawable.nen, "ss", "sss"));
+        anUongList.add(new AnUong(R.drawable.atiso, R.drawable.nen, "ss", "ssfs"));
+        anUongList.add(new AnUong(R.drawable.atiso, R.drawable.nen, "sds", "sdss"));
+        anUongList.add(new AnUong(R.drawable.atiso, R.drawable.nen, "sds", "sssss"));
+
+        adapter.addFragment(new ThucPhamFragment(anUongList), "Thực phẩm", R.drawable.mushroom);
+        adapter.addFragment(new ThucPhamFragment(anUongList), "Món ăn", R.drawable.muffin);
+        adapter.addFragment(new ThucPhamFragment(anUongList), "Nhóm chất", R.drawable.chart);
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
@@ -69,4 +81,12 @@ public class AnUongActivity extends AppCompatActivity {
         tab.setCustomView(adapter.getSelectedTabView(position));
     }
 
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(AnUongActivity.this, MenuMainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.trans_right_in,
+                R.anim.trans_right_out);
+        finish();
+    }
 }
--- app/src/main/java/haui/lethanhha/com/mothercare/fragment/ThucPhamFragment.java
+++ app/src/main/java/haui/lethanhha/com/mothercare/fragment/ThucPhamFragment.java
@@ -7,15 +7,19 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 import haui.lethanhha.com.mothercare.R;
+import haui.lethanhha.com.mothercare.activity.MenuMainActivity;
 import haui.lethanhha.com.mothercare.adapter.AnUongAdapter;
+import haui.lethanhha.com.mothercare.adapter.RecyclerViewOnClickListener;
 import haui.lethanhha.com.mothercare.model.AnUong;
 
-public class ThucPhamFragment extends Fragment {
+public class ThucPhamFragment extends Fragment implements RecyclerViewOnClickListener {
 
     private List<AnUong> anUongList;
 
@@ -23,16 +27,25 @@ public class ThucPhamFragment extends Fragment {
 
     private AnUongAdapter anUongAdapter;
 
-    public ThucPhamFragment(/*List<AnUong> anUongList*/) {
-     /*   this.anUongList = anUongList;*/
+    public ThucPhamFragment(List<AnUong> anUongList) {
+        this.anUongList = anUongList;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_an_uong, container, false);
-//        recyclerViewAnUong = view.findViewById(R.id.recyclerAnUong);
-//        anUongAdapter = new AnUongAdapter();
+        recyclerViewAnUong = view.findViewById(R.id.recyclerAnUong);
+        anUongAdapter = new AnUongAdapter(anUongList, getContext(), this);
+
+        recyclerViewAnUong.setAdapter(anUongAdapter);
+        recyclerViewAnUong.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewAnUong.setHasFixedSize(true);
         return view;
+    }
+
+    @Override
+    public void onClick(int position) {
+
     }
 }
